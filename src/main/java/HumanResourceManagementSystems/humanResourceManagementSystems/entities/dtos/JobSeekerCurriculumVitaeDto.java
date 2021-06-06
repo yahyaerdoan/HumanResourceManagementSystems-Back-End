502a737b213d23ceefa2d5c7782cd8c0365c5195
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2021-06-06 21:27:37 +0300

    DTO nesnesi oluşturuldu. Bazı Entity nesnelerinin tablo isimleri düzeltildi.

---
+++ src/main/java/HumanResourceManagementSystems/humanResourceManagementSystems/entities/dtos/JobSeekerCurriculumVitaeDto.java
@@ -0,0 +1,31 @@
+package HumanResourceManagementSystems.humanResourceManagementSystems.entities.dtos;
+
+import java.util.List;
+
+import HumanResourceManagementSystems.humanResourceManagementSystems.entities.concretes.CoverLetter;
+import HumanResourceManagementSystems.humanResourceManagementSystems.entities.concretes.Experience;
+import HumanResourceManagementSystems.humanResourceManagementSystems.entities.concretes.ForeignLanguage;
+import HumanResourceManagementSystems.humanResourceManagementSystems.entities.concretes.Image;
+import HumanResourceManagementSystems.humanResourceManagementSystems.entities.concretes.Jobseeker;
+import HumanResourceManagementSystems.humanResourceManagementSystems.entities.concretes.Link;
+import HumanResourceManagementSystems.humanResourceManagementSystems.entities.concretes.ProgrammingSkill;
+import HumanResourceManagementSystems.humanResourceManagementSystems.entities.concretes.School;
+
+public class JobSeekerCurriculumVitaeDto {
+
+	public Jobseeker jobseeker;
+	
+	public List<School> schools;
+	
+	public List<Experience> experiences;
+	
+	public List<ForeignLanguage> foreignLanguages;
+	
+	public List<Link> links;
+	
+	public List<ProgrammingSkill> programingSkills;
+	
+	public List<CoverLetter> coverLetter;
+	
+	public Image image;
+}
