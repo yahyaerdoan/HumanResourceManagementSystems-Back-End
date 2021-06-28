658115f8906235e44774e7532f5634f76d34d156
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2021-06-28 21:14:15 +0300

    Kullanılmayan importlar kaldırıldı.

--- src/main/java/HumanResourceManagementSystems/humanResourceManagementSystems/dataAccess/abstracts/JobSeekerDao.java
+++ src/main/java/HumanResourceManagementSystems/humanResourceManagementSystems/dataAccess/abstracts/JobSeekerDao.java
@@ -3,7 +3,6 @@ package HumanResourceManagementSystems.humanResourceManagementSystems.dataAccess
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import HumanResourceManagementSystems.humanResourceManagementSystems.entities.concretes.JobSeeker;
-import HumanResourceManagementSystems.humanResourceManagementSystems.entities.dtos.JobSeekerCurriculumVitaeDto;
 
 public interface JobSeekerDao extends JpaRepository<JobSeeker, Integer> {
 
